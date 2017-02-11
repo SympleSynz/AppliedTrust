8efe68b769640d863ba3cecf3c662505cdb0abdb
Michael Swisher <Michael.V.Swisher@colorado.edu>
2017-02-11 13:46:16 -0500

    Beginning the authentication service. Updating the ignore to ignore temp files.

---
+++ auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from Crypto.Cipher import AES
+from Crypto import Random
+
+import cmd2 as cmd
+
+import argparse
+import binascii
+import datetime
+import hashlib
+import getpass
+import logging
+import base64
+import time
+import sys
+import os
+
+
+
